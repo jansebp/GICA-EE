2276bb828609135eb01041b6d4f8872d44b2bf1e
Janse Brasileiro <jansebp@gmail.com>
2014-06-29 19:44:05 -0300

    Commit Inicial

---
+++ GICAEE.ImageViewer.PicBox/PicBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+
+namespace GICAEE.ImageViewer
+{
+    [DefaultProperty("Image")]
+    [ToolboxBitmap(typeof(PicBox), "PicBox.bmp")]
+    [ToolboxItem(true)]
+	
+	static class Constantes{
+        private const int MaxZoom = 3000;
+		private const int MinZoom = 1;
+    }
+    
+		public class PicBox
+		{
+		}
+	}
+}
---
+++ GICAEE.ImageViewer/DefaultForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GICAEE.ImageViewer
+{
+    public partial class DefaultForm : Form
+    {
+        #region Construtores
+        
+        public DefaultForm()
+        {
+            this.InitializeComponent();
+        }
+        
+        #endregion 
+
+        #region Outros Métodos
+
+        protected string FormatPoint(Point point)
+        {
+            return string.Format("X:{0}, Y:{1}", point.X, point.Y);
+        }
+
+        protected string FormatRectangle(RectangleF rect)
+        {
+            return string.Format("X:{0}, Y:{1}, W:{2}, H:{3}", (int)rect.X, (int)rect.Y, (int)rect.Width, (int)rect.Height);
+        }
+
+        #endregion
+    }
+}
